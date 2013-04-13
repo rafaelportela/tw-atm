e566634df3cff9e931a912d8994703374a194446
Luan <luan.rpsantos@gmail.com>
2013-04-13 12:14:04 -0300

    Added new kinds of accounts (current account and savings account) inherited from Account class.

---
+++ src/com/tw/atm/account/CurrentAccount.java
@@ -0,0 +1,9 @@
+package com.tw.atm.account;
+
+public class CurrentAccount extends Account {
+
+	public CurrentAccount(double balance) {
+		super(balance);
+	}
+
+}
---
+++ src/com/tw/atm/account/SavingsAccount.java
@@ -0,0 +1,9 @@
+package com.tw.atm.account;
+
+public class SavingsAccount extends Account {
+
+	public SavingsAccount(double balance) {
+		super(balance);
+	}
+
+}
