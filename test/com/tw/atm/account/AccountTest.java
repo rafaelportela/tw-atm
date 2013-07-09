df641e5eda7850d028552c4211c08b4e27f24ea8
Luan Reis <luan.rpsantos@gmail.com>
2013-07-09 16:02:25 -0300

    Created custom exceptions for nevative values and insufficient balance.

--- test/com/tw/atm/account/AccountTest.java
+++ test/com/tw/atm/account/AccountTest.java
@@ -15,47 +15,50 @@ public class AccountTest {
 	}
 
 	@Test
-	public void depositAmount() {
+	public void depositAmount() throws NegativeValueException {
 		double amount = 1000.0;
 		account.deposit(amount);
 		assertEquals(1000.0, account.getBalance(), 0.1);
 	}
 
-	@Test(expected = IllegalArgumentException.class)
-	public void throwsErrorForNegativeDeposit() {
+	@Test(expected = NegativeValueException.class)
+	public void throwsErrorForNegativeDeposit() throws NegativeValueException {
 		double amount = -1000.0;
 		account.deposit(amount);
 	}
 
 	@Test
-	public void withdrawMoney() {
+	public void withdrawMoney() throws NegativeValueException,
+			InsufficientBalanceException {
 		double amount = 1500.0;
 		account.deposit(amount);
 		account.withdraw(amount);
 		assertEquals(0.0, account.getBalance(), 0.1);
 	}
 
-	@Test(expected = IllegalArgumentException.class)
-	public void throwsErrorForInsufficientBalance() {
+	@Test(expected = InsufficientBalanceException.class)
+	public void throwsErrorForInsufficientBalance()
+			throws NegativeValueException, InsufficientBalanceException {
 		double amount = 500.0;
 		account.withdraw(amount);
 	}
 
-	@Test(expected = IllegalArgumentException.class)
-	public void throwsErrorForNegativeWithdraw() {
+	@Test(expected = NegativeValueException.class)
+	public void throwsErrorForNegativeWithdraw() throws NegativeValueException,
+			InsufficientBalanceException {
 		double amount = -2050.0;
 		account.withdraw(amount);
 	}
 
 	@Test
-	public void transferMoneyBetweenAccounts() {
+	public void transferMoneyBetweenAccounts() throws NegativeValueException {
 		Account sourceAccount = new Account(AccountType.CURRENT_ACCOUNT);
 		Account destinationAccount = new Account(AccountType.SAVINGS_ACCOUNT);
 
 		sourceAccount.deposit(100.0);
 
 		double amount = 50.0;
-		sourceAccount.transferMoney(amount, destinationAccount);
+		sourceAccount.transferMoney(destinationAccount, amount);
 
 		assertEquals(50.0, sourceAccount.getBalance(), 0.1);
 		assertEquals(50.0, destinationAccount.getBalance(), 0.1);
